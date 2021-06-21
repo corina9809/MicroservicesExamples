a3dcdc94cb8c56e394a60092c3a0ba68f6e6de4b
corina9809 <mypost333@web.de>
2021-06-21 13:29:13 +0200

    First Commit

---
+++ complete/hello-service/src/main/java/serviceregistrationanddiscoveryhello/HelloObject.java
@@ -0,0 +1,13 @@
+package serviceregistrationanddiscoveryhello;
+
+public class HelloObject {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ complete/hello-web-client-service/src/main/java/serviceregistrationanddiscoveryhelloclient/HelloObject.java
@@ -0,0 +1,13 @@
+package serviceregistrationanddiscoveryhelloclient;
+
+public class HelloObject {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
